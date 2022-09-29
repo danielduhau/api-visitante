eb3c67de4c7d91346b4846d45dc3107be5c09b0e
daniel.manhaes <daniel.manhaes@prodam.am.gov.br>
2022-09-29 11:11:58 -0400

    Commit inicial da API REST de visitantes.

---
+++ src/main/java/br/com/danielduhau/apivisitante/ApiVisitanteApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielduhau.apivisitante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVisitanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVisitanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danielduhau/apivisitante/ApiVisitanteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielduhau.apivisitante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVisitanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
