91cad747879277a2b13c811e2e706e44efaa92a8
daniel.manhaes <daniel.manhaes@prodam.am.gov.br>
2022-09-29 14:59:27 -0400

    Inclusão de TDD e rest-assured.

--- src/test/java/br/com/danielduhau/apivisitante/ApiVisitanteApplicationTests.java
+++ src/test/java/br/com/danielduhau/apivisitante/ApiVisitanteApplicationTests.java
@@ -1,5 +1,6 @@
 package br.com.danielduhau.apivisitante;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class ApiVisitanteApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertDoesNotThrow(() -> ApiVisitanteApplication.main(new String[]{}));
 	}
 
 }
